5ccd8c6a03d019b453cbff9ef19ec69f7b076274
Karl Henderson <karl@braqs.co>
2025-05-12 23:27:29 +0200

    progress on the map page
    added buttons, map view, and landing coordinates for miami

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,14 +1,34 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet, TouchableOpacity } from 'react-native';
 
 import EditScreenInfo from '@/components/EditScreenInfo';
 import { Text, View } from '@/components/Themed';
+import MapView, {Marker} from 'react-native-maps';
+import { Ionicons } from '@expo/vector-icons';
+
 
 export default function TabOneScreen() {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="app/(tabs)/index.tsx" />
+      <MapView style={styles.map}
+        initialRegion={{
+          latitude: 25.78,  // Miami
+          longitude: -80.20,
+          latitudeDelta: 0.06,
+          longitudeDelta: 0.035,
+        }}>
+      </MapView>
+      <TouchableOpacity style={styles.filterButton}>
+        <Ionicons name="filter" size={24} color="black" />
+      </TouchableOpacity>
+      <TouchableOpacity style={styles.plusButton}>
+        <Ionicons name="add" size={24} color="white" />
+      </TouchableOpacity>
+      <TouchableOpacity style={styles.searchButton}>
+        <Ionicons name="search" size={24} color="black" />
+      </TouchableOpacity>
+      <TouchableOpacity style={styles.locateButton}>
+        <Ionicons name="location" size={24} color="black" />
+      </TouchableOpacity>
     </View>
   );
 }
@@ -27,5 +47,61 @@ const styles = StyleSheet.create({
     marginVertical: 30,
     height: 1,
     width: '80%',
+  },
+  map: {
+    width: '100%',
+    height: '100%',
+  },
+  filterButton: {
+    position: 'absolute',
+    bottom: 120,
+    left: 20,
+    backgroundColor: 'white',
+    padding: 14,
+    borderRadius: 28,
+    elevation: 5, // Android shadow
+    shadowColor: '#000', // iOS shadow
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.3,
+    shadowRadius: 4,
+  },
+  plusButton: {
+    position: 'absolute',
+    bottom:120,
+    right: 20,
+    backgroundColor: '#1E3A8A',
+    padding: 18,
+    borderRadius: 36,
+    elevation: 5,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.3,
+    shadowRadius: 4,
+  },
+  searchButton: {
+    position: 'absolute',
+    bottom: 30,
+    left: 20,
+    backgroundColor: '#FFF',
+    padding: 18,
+    borderRadius: 36,
+    elevation: 5,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.3,
+    shadowRadius: 4,
+  },
+  locateButton: {
+    position: 'absolute',
+    bottom: 30,
+    right: 20,
+    backgroundColor: '#FFF',
+    padding: 18,
+    borderRadius: 36,
+    elevation: 5,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.3,
+    shadowRadius: 4,
   },
 });
